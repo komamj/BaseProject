19d9c5a1b319e0e77f3ac20c3176ee61304ed4c0
Wang Xin <xinxwang@thoughtworks.com>
2019-10-30 20:00:22 +0800

    Add MainActivity Test

--- app/src/main/java/com/oppo/cac/base/binding/DataBindingAdapters.kt
+++ app/src/main/java/com/oppo/cac/base/binding/DataBindingAdapters.kt
@@ -3,14 +3,13 @@ package com.oppo.cac.base.binding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.oppo.cac.base.BuildConfig
 import com.oppo.cac.base.R
 
-// TODO
-private const val THUMBNAIL_URL = "http://139.198.4.126:8080/image/"
 
 @Suppress("NAME_SHADOWING")
 @BindingAdapter("image_url")
 fun bindImageViewUrl(imageView: ImageView, thumbnailUrl: String?) {
-    val thumbnailUrl = "$THUMBNAIL_URL$thumbnailUrl"
+    val thumbnailUrl = "${BuildConfig.THUMBNAIL_URL}$thumbnailUrl"
     Glide.with(imageView).load(thumbnailUrl).placeholder(R.color.colorPrimary).into(imageView)
 }
